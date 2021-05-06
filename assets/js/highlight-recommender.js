c1e252df26854adc0bcc2387c346971b6e0cb634
Preston Lim <prestonlimlianjie@gmail.com>
2021-05-06 18:11:27 +0800

    feat: add utm trackers

--- assets/js/highlight-recommender.js
+++ assets/js/highlight-recommender.js
@@ -1,9 +1,9 @@
 function getUri(){
   let experimentId = 'boa-test'
   let userId = document.cookie
-    .split('; ')
-    .find(row => row.startsWith('_ga'))
-    .split('=')[1];
+  .split('; ')
+  .find(row => row.startsWith('_ga'))
+  .split('=')[1];
   let apiBaseUrl = "https://4zcksddi64.execute-api.ap-southeast-1.amazonaws.com/beta/highlight-recommender-staging"
   return `${apiBaseUrl}?userId=${userId}&experimentId=${experimentId}`
 }
@@ -13,9 +13,11 @@ axios.get(getUri())
   const keyHighlightsContainer = document.getElementById('key-highlights')
   let innerHtmlString = '<div class="bp-container"><div class="row is-gapless has-text-centered">'
   
+  const experimentId = response.data.experiment_id
+  const treatmentId = response.data.treatment_id
   response.data.recommended_highlights.forEach(function (highlight) {
     const { title, url } = highlight
-    innerHtmlString += `<div class="col"><a href="${url}" class="is-highlight"><div class="key-highlights-text"><p class="has-text-weight-semibold has-text-white key-highlight-title is-uppercase padding--top--xs">${title}</p></div></a></div>`
+    innerHtmlString += `<div class="col"><a href="${url}?utm_source=${experimentId}&utm_medium=${treatmentId}" class="is-highlight"><div class="key-highlights-text"><p class="has-text-weight-semibold has-text-white key-highlight-title is-uppercase padding--top--xs">${title}</p></div></a></div>`
   })
   
   innerHtmlString += '</div></div>'

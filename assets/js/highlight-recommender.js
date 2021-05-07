2b542356e6b840a39728f84b1f18d400fcf4afdc
Preston Lim <prestonlimlianjie@gmail.com>
2021-05-07 11:30:55 +0800

    fix

--- assets/js/highlight-recommender.js
+++ assets/js/highlight-recommender.js
@@ -17,7 +17,7 @@ axios.get(getUri())
   const treatmentId = response.data.treatment_id
   response.data.recommended_highlights.forEach(function (highlight) {
     const { title, url, description } = highlight
-    innerHtmlString += `<div class="col"><a href="${url}?utm_source=${experimentId}&utm_medium=${treatmentId}" class="is-highlight"><div class="key-highlights-text"><p class="has-text-weight-semibold has-text-white key-highlight-title is-uppercase padding--top--xs">${title}</p></div></a></div><p class="has-text-white-trans padding--bottom--sm">${description}</p>`
+    innerHtmlString += `<div class="col"><a href="${url}?utm_source=${experimentId}&utm_medium=${treatmentId}" class="is-highlight"><div class="key-highlights-text"><p class="has-text-weight-semibold has-text-white key-highlight-title is-uppercase padding--top--xs">${title}</p><p class="has-text-white-trans padding--bottom--sm">${description}</p></div></a>`
   })
   
   innerHtmlString += '</div></div>'
